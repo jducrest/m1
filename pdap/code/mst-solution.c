cb9c9b82863c5e3afcfb816788fd4a73cf920e61
Du crest Julien <jducrest@slsu0-19.dsi.ens-lyon.fr>
2017-12-09 17:52:24 +0100

    nice commit

--- pdap/code/mst-solution.c
+++ pdap/code/mst-solution.c
@@ -48,6 +48,12 @@ void computeMST(
 	 /*************************************/
 
 	 int i,j,n;
+	 for(i=0;i<N;i++)
+	 {
+		printf("\n");
+		for(j=0;j<N;j++)
+		printf("%d ",adj[i+N*j]);
+	 }
 	 int* T=malloc(sizeof(int)*N);
 	 int* D=malloc(sizeof(int)*N);
 	 int* Ne=malloc(sizeof(int)*N);
@@ -89,7 +95,7 @@ void computeMST(
 			{
 				umin = Ne[i];
 				vmin = i;
-end_datatype		}
+			}
 			if( !T[i] && D[i]==D[vmin] ) //potential candidate (need to check lex order)
 			{
 				int nei = Ne[i];
@@ -189,36 +195,67 @@ end_datatype		}
 	 /*************************************/
 	 /*                                   */
 	 /*     PRIM'S PAR ALGORITHM          */
-	 /*                                   */
+	 /*     goto pp                       */
 	 /*************************************/
-    if(! (N%p))
+    if((N%numProcs))
 		printf("we are fucked !\n");
-	 int sizeofdata = N*(N/p)
-	 int Ns = N/p;
+	 int sizeofdata = N*(N/numProcs);
+	 int Ns = N/numProcs;
 	 int* adjs = malloc(sizeof(int)*sizeofdata);
-	 MPI_Scatter(adj,sizeofdata,MPI_INT,ajds,sizeofdata,MPI_INT,0,MPI_COMM_WORLD);
-
+	 // Now everybody got his adj small
 	 int i,j,n;
+	 MPI_Barrier(MPI_COMM_WORLD);
+	 if(procRank==0)
+	 {
+	 for(i=0;i<N;i++)
+	 {
+	 printf("\n");
+	 for(j=0;j<N;j++)
+	 {
+		printf("%d ",adj[i+N*j]);
+	 }
+	 }
+	 }
+	 MPI_Barrier(MPI_COMM_WORLD);
+	 MPI_Scatter(adj,sizeofdata,MPI_INT,adjs,sizeofdata,MPI_INT,0,MPI_COMM_WORLD);
+	  
+	 for(n=0;n<numProcs;n++)
+	 {
+	   MPI_Barrier(MPI_COMM_WORLD);
+		if(procRank ==n)
+		{
+			printf("rank n%d:\n",procRank);
+			for(i=0;i<Ns;i++)
+			for(j=0;j<N;j++)
+			printf("%d\n",adjs[i*N+j]);
+
+		}
+	 }
+	 MPI_Barrier(MPI_COMM_WORLD);
 	 int* T=malloc(sizeof(int)*N);
-	 int* D=malloc(sizeof(int)*Ns);
-	 int* Ne=malloc(sizeof(int)*Ns);
-
-// TO BE CONTINUED
-
+	 int* D=malloc(sizeof(int)*(Ns+1));
+	 int* Ne=malloc(sizeof(int)*(Ns+1));
+	 // thoses are the candidates that we send back to proc 0 for check
+	 int* us = malloc(sizeof(int)*numProcs);
+	 int* vs = malloc(sizeof(int)*numProcs);
+	 int* ds = malloc(sizeof(int)*numProcs);
+		
 	 /*INIT**********************/
 	 /* 0 is in the tree        */
 	 /* everybody else is not   */
 	 /* we update the distances */
 	 /***************************/
-	 T[0] = 1;
-	 D[0] = INT_MAX;
-	 Ne[0] = INT_MAX;
-	 for(i=1;i<N;i++)
-	 {
+	 T[Ns] = 1;
+	 D[Ns] = INT_MAX;
+	 Ne[Ns] = INT_MAX;
+	 for(i=0;i<N;i++)
 		T[i] = 0;
-		if(adj[i+0*N] != 0)
+	 // Now everybody initialize his small thingies !!! Ns is just a init value, index range from 0 to <Ns
+	 for(i=0;i<Ns;i++)
+	 {
+		if(adjs[0+i*N] != 0)
 		{
-			D[i] = adj[i+0*N];
+			D[i] = adjs[0+i*N];
 			Ne[i] = 0;
 		}
 		else
@@ -227,6 +264,7 @@ end_datatype		}
 			Ne[i] = INT_MAX;
 		}
 	 }
+	 T[0] = 1;
 	 /* MAIN LOOP ******************************************/
 	 /* find n-1 edges to build the tree                   */
 	 /* we denote the edge by (u,v) where u is in the tree */
@@ -235,9 +273,9 @@ end_datatype		}
 	 /******************************************************/
 	 for(n=0;n<N-1;n++)
 	 {
-		int umin = 0;  
-		int vmin = 0;
-		for(i=1;i<N;i++)
+		int umin = Ns;  
+		int vmin = Ns;
+		for(i=0;i<Ns;i++)
 		{
 			if( !T[i] && D[i]<D[vmin] ) // clear candidate
 			{
@@ -256,15 +294,42 @@ end_datatype		}
 				}
 			}
 		}
-		/* OUTPUT SOLUTION ****************/
-		printf("%d %d\n",MIN(umin,vmin),MAX(umin,vmin));
+		/* GATHER INFO */
+		MPI_Gather(&umin,1,MPI_INT,us,1,MPI_INT,0,MPI_COMM_WORLD);
+		MPI_Gather(&vmin,1,MPI_INT,vs,1,MPI_INT,0,MPI_COMM_WORLD);
+		MPI_Gather(&D[vmin],1,MPI_INT,ds,1,MPI_INT,0,MPI_COMM_WORLD);
+	   printf("my Dmin : %d!!!\n",D[vmin]); 	
+		int imin = 0;
+		if(procRank==0)
+		{
+			printf("\n the edges : \n");
+			for(i=0;i<numProcs;i++)
+				printf("%d %d %d\n",us[i],vs[i],ds[i]);
+			for(i=1;i<numProcs;i++)
+			{
+				if(ds[i]<ds[imin])
+					imin = i;
+				if(ds[i] == ds[imin])
+				if(MIN(us[i],vs[i]) < MIN(us[imin],vs[imin]) ||( MIN(us[i],vs[i]) == MIN(us[imin],vs[imin]) && MAX(us[i],vs[i])< MAX(us[imin],vs[imin])))
+					imin = i;
+			}
+			// we found the best guy
+			/* OUTPUT SOLUTION ****************/
+			printf("Arete! : %d %d\n",MIN(umin,vmin),MAX(umin,vmin));
+		}
+		// we send it to everybody
+		umin = us[imin];
+		vmin = vs[imin];
+		MPI_Bcast(&umin,1,MPI_INT,0,MPI_COMM_WORLD);
+		MPI_Bcast(&vmin,1,MPI_INT,0,MPI_COMM_WORLD);
+      //Now that everybody knows who it is we can print and update
 
 		/* UPDATE *****************************************/
 		/* we update T and D and V being careful with     */
 		/* lex order                                      */
 		/**************************************************/
 		T[vmin]=1;
-		for(j=1;j<N;j++)
+		for(j=0;j<Ns;j++)
 		{
 			if(adj[vmin*N+j]!=0 && adj[vmin*N+j]<D[j])
 			{
