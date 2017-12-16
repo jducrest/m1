e212254113be74ce4c702e4988f66c15dfdf9eb1
Du crest Julien <jducrest@slsu0-19.dsi.ens-lyon.fr>
2017-12-16 13:10:14 +0100

    nice commit

--- pdap/code/prim.c
+++ pdap/code/prim.c
@@ -1,80 +1,73 @@
-/*************************************/
-	 /*                                   */
-	 /*         PRIM'S ALGORITHM          */
-	 /*                                   */
-	 /*************************************/
+	/*************************************/
+	/*                                   */
+	/*         PRIM'S ALGORITHM          */
+	/*                                   */
+	/*************************************/
 
-	 int i,j,n;
-	 int* T=malloc(sizeof(int)*N);
-	 int* D=malloc(sizeof(int)*N);
-	 int* Ne=malloc(sizeof(int)*N);
-	 /*INIT**********************/
-	 /* 0 is in the tree        */
-	 /* everybody else is not   */
-	 /* we update the distances */
-	 /***************************/
-	 T[0] = 1;
-	 Ne[0] = INT_MAX;
-	 D[0] = INT_MAX;
-	 for(i=1;i<N;i++)
-	 {
+	int i,j,n,umin,vmin;
+	edge e,emin;
+	int* T=malloc(sizeof(int)*N);
+	int* D=malloc(sizeof(int)*N);
+	int* Ne=malloc(sizeof(int)*N);
+	/*INIT**********************/
+	/* 0 is in the tree        */
+	/* everybody else is not   */
+	/* we update the distances */
+	/***************************/
+	T[0] = 1;
+	Ne[0] = INT_MAX;
+	D[0] = INT_MAX;
+	/**************************************************/
+	/* way easier to see it as a graph with           */
+	/* infinite distance between non-connected edges. */
+	/**************************************************/
+	for(i=0;i<N;i++)
+		for(j=0;j<N;j++)
+			if(W(i,j)==0)
+				W(i,j)=INT_MAX;
+
+	for(i=1;i<N;i++)
+	{
 		T[i] = 0;
-		if(W(0,i)!= 0)
-		{
-			D[i] = W(0,i);
-			Ne[i] = 0;
-		}
-		else
-		{
-			D[i] = INT_MAX;
-			Ne[i] = INT_MAX;
-		}
-	 }
-	 /* MAIN LOOP ******************************************/
-	 /* find n-1 edges to build the tree                   */
-	 /* we denote the edge by (u,v) where u is in the tree */
-	 /* at the beginning, umin = 0, vmin = 0, dmin=INT_MAX */
-	 /* this ensure that the first existing edge is picked */
-	 /******************************************************/
-	 for(n=0;n<N-1;n++)
-	 {
-		int umin = 0;  
-		int vmin = 0;
-		int u,v,w;
+		D[i] = W(0,i);
+		Ne[i] = 0;
+	}
+	/* MAIN LOOP ******************************************/
+	/* find n-1 edges to build the tree                   */
+	/* we denote the edge by (u,v) where u is in the tree */
+	/* at the beginning, umin = 0, vmin = 0, dmin=INT_MAX */
+	/* this ensure that the first existing edge is picked */
+	/******************************************************/
+	for(n=0;n<N-1;n++)
+	{
+		emin.w=INT_MAX;
 		for(i=1;i<N;i++)
 		{
-			edge e1;
-			edge e2;
-			e1.i = Ne[i];
-			e1.j = i;
-			e1.w = D[i];
-			e2.i=umin;
-			e2.j=vmin;
-			e2.w = D[vmin];
-			if(!T[i] && sed_lex(&e2,&e1))
-			{
-				umin = Ne[i];
-				vmin = i;
-			}
+			e.i = Ne[i];
+			e.j = i;
+			e.w = D[i];
+			if(!T[i] && sed_lex(&emin,&e))
+				emin = e;
 		}
 		/* OUTPUT SOLUTION ****************/
-		printf("%d %d\n",MIN(umin,vmin),MAX(umin,vmin));
+		printf("%d %d\n",MIN(emin.i,emin.j),MAX(emin.i,emin.j));
 
 		/* UPDATE *****************************************/
 		/* we update T and D and V being careful with     */
 		/* lex order                                      */
 		/**************************************************/
-		T[vmin]=1;
+		i = emin.j;
+		T[i]=1;
 		for(j=1;j<N;j++)
 		{
-			if(W(vmin,j)!=0 && W(vmin,j)<D[j])
+			if(W(i,j)<D[j])
 			{
-				D[j] = W(vmin,j);
-				Ne[j] = vmin;
+				D[j] = W(i,j);
+				Ne[j] = i;
 			}
-			if(W(vmin,j)!=0 && W(vmin,j)==D[j])
+			if(W(i,j)==D[j])
 			{
-				Ne[j] = MIN(vmin,Ne[j]);
+				Ne[j] = MIN(i,Ne[j]);
 			}
 		}
 	 }
