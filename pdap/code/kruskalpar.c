2a1e764e55ccd8731c9a9799fb74bc15687cdd2d
Du crest Julien <jducrest@slsu0-19.dsi.ens-lyon.fr>
2017-12-16 17:56:17 +0100

    nice commit

--- pdap/code/kruskalpar.c
+++ pdap/code/kruskalpar.c
@@ -1,35 +1,31 @@
-/*********************************************/
-	 /*                                           */
-	 /*               KRUSKAL PAR                 */
-    /*               goto kp                     */
-	 /*********************************************/
-	 
-	 int Ns = procRank != numProcs - 1 ? ceil((float)N/numProcs) : N - ceil((float)N/numProcs)*(numProcs-1);
-	 int offset = procRank * ceil((float)N / numProcs);
+	/*********************************************/
+	/*                                           */
+	/*               KRUSKAL PAR                 */
+   /*               goto kp                     */
+	/*********************************************/
+	
+	int Ns = procRank != numProcs - 1 ? ceil((float)N/numProcs) : N - ceil((float)N/numProcs)*(numProcs-1);
+	int offset = procRank * ceil((float)N / numProcs);
 
-	 int i,j,k,n,min,imin;
-	 // S is the representant set, E the set of edges
-	 int*  S  = malloc(sizeof(int)*N);
-	 edge* E  = (edge*) malloc(sizeof(edge)*MAX(M,N+1));
-	 edge* E1 = (edge*) malloc(sizeof(edge)*(N+1));
-	 edge* E2 = (edge*) malloc(sizeof(edge)*(N+1));
-	 int first_time = 1;
-	 int K;
-	 n=0;
-	 E1[0].i = INT_MAX;
-	 E2[0].i = INT_MAX;
-	 while(1)
-	 {
-	 if(first_time)
-	 {
-	 /* PHASE 1 : **********************************/
-	 /* we initialize E using the adjacency matrix */
-	 /**********************************************/
-	 first_time = 0;
-	 k = 0;
-	 for(i=0;i<Ns;i++)
-	 for(j=offset+i;j<N;j++)
-	 {
+	int i,j,k,n,min,imin;
+	// S is the representant set, E the set of edges
+	int*  S  = malloc(sizeof(int)*N);
+	edge* E  = (edge*) malloc(sizeof(edge)*MAX(M,N+1));
+	edge* E1 = (edge*) malloc(sizeof(edge)*(N+1));
+	edge* E2 = (edge*) malloc(sizeof(edge)*(N+1));
+	int first_time = 1;
+	int K;
+	n=0;
+	E1[0].i = INT_MAX;
+	E2[0].i = INT_MAX;
+	/* PHASE 1 : **********************************/
+	/* we initialize E using the adjacency matrix */
+	/**********************************************/
+	first_time = 0;
+	k = 0;
+	for(i=0;i<Ns;i++)
+	for(j=offset+i;j<N;j++)
+	{
 		if(W(i,j)!=0)
 		{
 			E[k].i = MIN(i+offset,j);
@@ -37,16 +33,16 @@
 			E[k].w = W(i,j);
 			k++;
 		}
-	 }
-	 E[k].i = INT_MAX;
-	 K = k;
-	 }
-	 else
-	 {
-	 /* PHASE 1 bis: ************************************************/
-	 /* we initialize E using E1 and E2                             */
-	 /* we do the return cards and see what happens method to get E */
-	 /***************************************************************/
+	}
+	E[k].i = INT_MAX;
+	K = k;
+	goto skipphaseone;
+	while(1)
+	{
+		/* PHASE 1 bis: ************************************************/
+		/* we initialize E using E1 and E2                             */
+		/* we do the return cards and see what happens method to get E */
+		/***************************************************************/
 		i=0;j=0;k=0;
 		while(1)
 		{
@@ -81,37 +77,37 @@
 			k++;
 		}
 		K=k;
-	 }
 
-	 /* PHASE 2 : **********************************/
-	 /* we initialize S and go for it!             */
-	 /**********************************************/
-	 if(procRank%(1<<n)==0)
-	 {
-	 for(i=0;i<N;i++)
-		S[i]=i;
-	 qsort(E,K,sizeof(edge),sed_lex);
-	i=0;k=0;
-	while(k<K)
-	{
-		int x = E[k].i;
-		int y = E[k].j;
-		int w = E[k].w;
-		while(S[x]!=S[S[x]])
-			S[x] = S[S[x]];
-		while(S[y]!=S[S[y]])
-			S[y] = S[S[y]];
-		if(S[x]!=S[y])
+		skipphaseone:
+		/* PHASE 2 : **********************************/
+		/* we initialize S and go for it!             */
+		/**********************************************/
+		if(procRank%(1<<n)==0)
 		{
-			S[S[MAX(x,y)]]=MIN(x,y);
-			E1[i]=E[k];
-			i++; // we found a good edge... happy =)
+			for(i=0;i<N;i++)
+				S[i]=i;
+		qsort(E,K,sizeof(edge),sed_lex);
+		i=0;k=0;
+		while(k<K)
+		{
+			int x = E[k].i;
+			int y = E[k].j;
+			int w = E[k].w;
+			while(S[x]!=S[S[x]])
+				S[x] = S[S[x]];
+			while(S[y]!=S[S[y]])
+				S[y] = S[S[y]];
+			if(S[x]!=S[y])
+			{
+				S[S[MAX(x,y)]]=MIN(x,y);
+				E1[i]=E[k];
+				i++; // we found a good edge... happy =)
+			}
+			k++;// we prepare to look into next edge
 		}
-		k++;// we prepare to look into next edge
-	}
    	E1[i].i = INT_MAX; // this is a sign the table ended there
-    }
-	 if( ( 1 << n ) >= numProcs)
+   }
+	if( ( 1 << n ) >= numProcs)
 		break;
 	if(procRank%(2<<n)==(1<<n)) // i'm the kind of guy who likes to send!
 	{
@@ -128,7 +124,6 @@
 	{
 		for(i=0;i<N-1;i++)
 			printf("%d %d\n",E1[i].i,E1[i].j);
-			//printf("%d %d %d\n", E1[i].w, E1[i].i, E1[i].j);
 	}
 	
 	free(S);
--- pdap/code/primpar.c
+++ pdap/code/primpar.c
@@ -1,127 +1,82 @@
-/*************************************/
-	 /*                                   */
-	 /*     PRIM'S PAR ALGORITHM          */
-	 /*     goto pp                       */
-	 /*************************************/
-	 int Ns = procRank != numProcs - 1 ? ceil((float)N/numProcs) : N - ceil((float)N/numProcs)*(numProcs-1);
-	 int offset = procRank * ceil((float)N / numProcs);
-	 // Now everybody got his adj small
+	/*************************************/
+	/*                                   */
+	/*     PRIM'S PAR ALGORITHM          */
+	/*     goto pp                       */
+	/*************************************/
+	int Ns = procRank != numProcs - 1 ? ceil((float)N/numProcs) : N - ceil((float)N/numProcs)*(numProcs-1);
+	int offset = procRank * ceil((float)N / numProcs);
 
-	 int i,j,n;
-    edge e;
-	 int* T=malloc(sizeof(int)*Ns);
-	 int* D=malloc(sizeof(int)*Ns);
-	 int* Ne=malloc(sizeof(int)*Ns);
-	 // thoses are the candidates that we send back to proc 0 for check
-	 int* us = malloc(sizeof(int)*numProcs);
-	 int* vs = malloc(sizeof(int)*numProcs);
-	 int* ds = malloc(sizeof(int)*numProcs);
+	int i,j,n;
+   edge e,emin;
+	int* T=malloc(sizeof(int)*Ns);
+	int* D=malloc(sizeof(int)*Ns);
+	int* Ne=malloc(sizeof(int)*Ns);
 		
-	 /*INIT**********************/
-	 /* 0 is in the tree        */
-	 /* everybody else is not   */
-	 /* we update the distances */
-	 /***************************/ 
-	 W(0,offset) = INT_MAX; // this is used in case we can't fine an edge to add
+	/*INIT**********************/
+	/* 0 is in the tree        */
+	/* everybody else is not   */
+	/* we update the distances */
+	/***************************/ 
+	for(i=0;i<Ns;i++)
+		for(j=0;j<N;j++)
+			if(W(i,j)==0)
+				W(i,j) = INT_MAX;
+	
+	W(0,offset) = INT_MAX; // this is used in case we can't fine an edge to add
 	 
-	 for(i=0;i<Ns;i++)
-	 {
+	// Now everybody initialize his small thingies !!! Ns is just a init value, index range from 0 to <Ns
+	for(i=0;i<Ns;i++)
+	{
 		T[i] = 0;
-		D[i] = INT_MAX;
-		Ne[i] = INT_MAX;
-	 }
-	 // Now everybody initialize his small thingies !!! Ns is just a init value, index range from 0 to <Ns
-	 for(i=0;i<Ns;i++)
-	 {
-		if(W(i,0) != 0)
-		{
-			D[i] = W(i,0);
-			Ne[i] = 0;
-		}
-	 }
-	 if(procRank==0)
-	 {
+		D[i] = W(i,0);
+		Ne[i] = 0;
+	}
+	if(procRank==0)
 		T[0] = 1;
-	 }
-	 /* MAIN LOOP ******************************************/
-	 /* find n-1 edges to build the tree                   */
-	 /* we denote the edge by (u,v) where u is in the tree */
-	 /* at the beginning, umin = 0, vmin = 0, dmin=INT_MAX */
-	 /* this ensure that the first existing edge is picked */
-	 /******************************************************/
+	
+	/* MAIN LOOP ******************************************/
+	/* find n-1 edges to build the tree                   */
+	/* we denote the edge by (u,v) where u is in the tree */
+	/* at the beginning, umin = 0, vmin = 0, dmin=INT_MAX */
+	/* this ensure that the first existing edge is picked */
+	/******************************************************/
 	 
-	 for(n=0;n<N-1;n++)
-	 {
-		int umin,vmin,dmin;
+	for(n=0;n<N-1;n++)
+	{
 		// we find a potential edge
+		emin.i=INT_MAX;
+		emin.j=INT_MAX;
+		emin.w=INT_MAX;
 		for(i=0;i<Ns;i++)
-		{
 			if(!T[i])
-			{
-				umin = Ne[i];
-				vmin = i+offset;
-				break;
+			{	
+				e.i = i+offset;	e.j = Ne[i];	e.w = W(i,Ne[i]);
+			
+				if(sed_lex(&emin,&e))
+					emin=e;
 			}
-		}
-		if(i==Ns) // we couldn't find an edge give back error value
-		{
-			umin = INT_MAX;
-			vmin = INT_MAX;
-			dmin = INT_MAX;
-			goto gatherpp;
-		}
-		// for the rest we compare to what we know
-		for(i=i;i<Ns;i++)
-		{
-			if(!T[i] && D[i]<D[vmin-offset] ) // clear candidate
-			{
-				umin = Ne[i];
-				vmin = i+offset;
-			}
-			if(!T[i] && D[i]==D[vmin-offset] ) //potential candidate (need to check lex order)
-			{
-				int nei = Ne[i];
-				int u = MIN(i+offset,nei);
-				int v = MAX(i+offset,nei);
-				if(u<MIN(umin,vmin) || (u==MIN(umin,vmin) && v<MAX(umin,vmin)))
-				{
-					umin = nei;
-					vmin = i+offset;
-				}
-			}
-		}
-		dmin = D[vmin-offset];
-		gatherpp:
 		
-		e.i = umin;
-		e.j = vmin;
-		e.w = dmin;
-		MPI_Allreduce(&e,&e,1,edge_type,edge_red,MPI_COMM_WORLD);
+		MPI_Allreduce(&emin,&emin,1,edge_type,edge_red,MPI_COMM_WORLD);
 		
 		/* UPDATE *****************************************/
 		/* we update T and D and V being careful with     */
 		/* lex order                                      */
 		/**************************************************/
-		umin = e.i;
-		vmin = e.j;
 		if(procRank==0)
-			printf("%d %d\n",MIN(umin,vmin),MAX(umin,vmin));
+			printf("%d %d\n",MIN(emin.i,emin.j),MAX(emin.i,emin.j));
 		
-		if(vmin/Ns==procRank) //it's somebody from this proc that was chosen
-	   {
-		   T[vmin-offset]=1;
-		}
+		if(emin.i/Ns==procRank) //it's somebody from this proc that was chosen
+		   T[emin.i-offset]=1;
+
 		for(j=0;j<Ns;j++)
 		{
-			if(W(j,vmin)!=0 && W(j,vmin)<D[j])
+			if(W(j,emin.i) < D[j])
 			{
-				D[j] = W(j,vmin);
-				Ne[j] = vmin;
+				D[j] = W(j,emin.i);
+				Ne[j] = emin.i;
 			}
-			if(W(j,vmin)!=0 && W(j,vmin)==D[j])
-			{
-				Ne[j] = MIN(vmin,Ne[j]);
-			}
+			if(W(j,emin.i)==D[j])
+				Ne[j] = MIN(emin.i,Ne[j]);
 		}
 	 }
 
